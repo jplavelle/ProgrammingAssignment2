3fc7ef25e29d8dbb12f5aecaad30f07b1cc92d1e
Jacob <jacob.p.lavelle@gmail.com>
2015-09-27 17:56:02 -0400

    This version contains comments in the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix function and cacheSolve function work together to
+## reduce the amount of time and processing power required to produce the
+## inverse of a matrix. Rather than computing the inverse of the same matrix
+## multiple times, this pair of functions checks if the inverse for the matrix
+## has already been computed. If so, the value is retrieved from the cache 
+## instead of running a new set of calculations. If not, the inverse matrix is
+## calculated, and the result is saved in the cache and returned to the user.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function contains four functions that the cacheSolve
+## function calls upon as needed to either return an existing value or, if one
+## does not exist, take the calcuated value passed to it from cacheSolve and
+## save it in the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
         Minv<-NULL
@@ -16,7 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The user calls the cacheSolve function and enters a matrix as the argument.
+## This function beings by creating an object 'z' that enables subsetting of
+## the makeCacheMatrix function. The cacheSolve function then calls the
+## relevant subsets to check if a value exists in the cache, return any
+## existing value, and, if no value is already saved, calculate the inverse and
+## save the result to cache while returning the value to the user.
+
 
 cacheSolve <- function(x, ...) {
         z<-makeCacheMatrix()
